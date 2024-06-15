96cdcf29a96964a0c6f319be043ae9273624db01
vando <do@dos-iMac.local>
2024-06-15 07:43:32 +0700

    update images

--- src/album.tsx
+++ src/album.tsx
@@ -1,14 +1,15 @@
 interface Album {
   title: string;
+  album: string;
   des?: string;
   created: string;
   urls: string[];
 }
 
-export default function ({ title, urls, created }: Album) {
+export default function ({ title, album, urls, created }: Album) {
   return (
     <div className="w-full h-[20em] border-2 text-black-dark hover:cursor-pointer relative shadow-md">
-      <img src={urls[0]} alt={title} className="shadow-mdy w-full h-full" />
+      <img src={`/albums/${album}/${urls[0]}.jpg`} alt={title} className="shadow-mdy w-full h-full" />
       <span className="absolute bottom-0 left-2">
         <h1>{title}</h1>
         <p>{created}</p>
