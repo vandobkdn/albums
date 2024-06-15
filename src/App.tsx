f05d6517bd36fdb1704d92d455c414f9923b80b5
vando <do@dos-iMac.local>
2024-06-15 07:09:58 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <h1 className="font-normal text-2xl">Bé 🐼 ❤️ Anh 😍</h1>
       </header>
 
-      <main className="grid gap-x-4 gap-y-20 xs:gap-y-5 sm:gap-y-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      <main className="grid gap-x-4 gap-y-20 xs:gap-y-5 sm:gap-y-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {albums.map((album, index) => (
           <Album key={index} {...album} />
         ))}
