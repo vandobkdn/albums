05c13c082c6db3839c8e5133c690be8b17bb27a4
vando <do@dos-iMac.local>
2024-06-26 07:41:25 +0700

    update

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,18 +3,22 @@ import { useAppContext } from '../../context';
 
 export const Header = () => {
   const {
-    setActiveLink,
-    state: { activeLink },
+    state: { albums, chosenAlbum },
+    selectAlbum,
   } = useAppContext();
 
-  const isWeddingActive = activeLink === 'wedding';
+  const weddingAlbum = albums.filter(album => album.type === 'wedding')[0];
+
+  const isWeddingActive = weddingAlbum.type === chosenAlbum?.type;
+
+  const handleSelect = () => selectAlbum(weddingAlbum);
 
   return (
     <header className="h-full w-full px-4 flex items-center justify-between">
       <Icon icon={Icons.Logo} />
       <button
         aria-label="Hình cưới"
-        onClick={() => setActiveLink('wedding')}
+        onClick={handleSelect}
         className={`${isWeddingActive && 'text-blue rounded-md bg-gray-200'} text-black text-sm p-1 px-2 hover:rounded-md hover:bg-gray-200 hover:text-blue`}
       >
         Hình cưới
